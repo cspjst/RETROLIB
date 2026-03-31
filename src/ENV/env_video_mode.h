5dc2cb6967c63ee0011632a75635686b50d9faac
Hellorld! <jeremy.thornton@nhs.net>
2026-03-31 08:38:22 +0100

    video mode

--- src/ENV/env_video_mode.h
+++ src/ENV/env_video_mode.h
@@ -1,6 +1,10 @@
 #ifndef ENV_VIDEO_MODE_H
 #define ENV_VIDEO_MODE_H
 
-//unsigned char env_set_video_mode(uns)
+#include "../../doslib/bioslib/src/BIOS/bios_video_services_types.h"
+
+bios_video_mode_t env_set_video_mode(bios_video_mode_t mode);
+
+bios_video_mode_t env_get_video_mode();
 
 #endif
