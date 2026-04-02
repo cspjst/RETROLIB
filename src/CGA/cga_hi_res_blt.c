66e3c05b80879183aee0b1bf682015fed76ca5e6
Sauce Code... <jeremy.thornton@nhs.net>
2026-04-02 10:14:10 +0100

    Add ES register push and pop in VRAM fill function

--- src/CGA/cga_hi_res_blt.c
+++ src/CGA/cga_hi_res_blt.c
@@ -6,14 +6,17 @@ void cga_hi_res_fill_vram(const char* data) {
     __asm {
         .8086
         push    ds
+        push    es
         pushf
 
-        lds     si, data
-        les     di, CGA_VRAM_PTR
-        cld
-        
+        lds     si, data                    ; DS:SI source RAM
+        les     di, CGA_VRAM_PTR            ; ES:DI destination VRAM
+        mov     cx, CGA_SCREEN_WORDS        ; 2000h words
+        cld                                 ; increment DI
+        rep     movsw                       ; copy 16K to VRAM
 
         popf
+        pop     es
         pop     ds
     }
 }
