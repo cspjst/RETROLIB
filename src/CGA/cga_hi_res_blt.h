8bdd8d017406c4d36e3ea2923a0db0badf25c7df
Sauce Code... <jeremy.thornton@nhs.net>
2026-04-02 11:19:46 +0100

    Update cga_hi_res_blt.h

--- src/CGA/cga_hi_res_blt.h
+++ src/CGA/cga_hi_res_blt.h
@@ -10,6 +10,6 @@
 
 void cga_hi_res_fill_vram(const char* data);
 
-void cga_hi_res_blt(cga_coord_t x, cga_coord_t y, cga_coord_t w, cga_coord_t h, const char* data);
+void __fastcall cga_hi_res_blt(cga_coord_t x, cga_coord_t y, cga_coord_t w, cga_coord_t h, const char* data);
 
 #endif
