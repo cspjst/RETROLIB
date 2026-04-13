6f24fdb3bb8b5f90967a77d750ee6ba2fdd23dca
Sauce Code... <jeremy.thornton@nhs.net>
2026-04-13 14:15:18 +0100

    Add cga_lo_res_blt.c with screen blit functions

---
+++ src/CGA/LO/cga_lo_res_blt.c
@@ -0,0 +1,13 @@
+#include "cga_lo_res_blt.h"
+
+void cga_lo_res_screen_blt(const char* data) {
+
+}
+
+void cga_lo_res_blt4x4(cga_coord_t x, cga_coord_t y, const char* bmp) {
+
+}
+
+void cga_lo_res_blt8x8(cga_coord_t x, cga_coord_t y, const char* bmp) {
+
+}
