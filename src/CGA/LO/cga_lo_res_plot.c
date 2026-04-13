f9ff0f075d9651d59a84f635d2215c337b601fd2
Sauce Code... <jeremy.thornton@nhs.net>
2026-04-13 13:47:41 +0100

    Refactor comments in cga_lo_res_plot function
    Updated assembly code comments for clarity and removed unnecessary comments.

--- src/CGA/LO/cga_lo_res_plot.c
+++ src/CGA/LO/cga_lo_res_plot.c
@@ -1,3 +1,8 @@
+/**
+ * @author      Jeremy Simon Thornton
+ * @copyright   2026 Jeremy Simon Thornton
+ * @version     0.1.0
+ */
 #include "cga_lo_res_plot.h"
 #include "../cga_constants.h"
 #include "../cga_lookup_table_y.h"
@@ -6,24 +11,20 @@ void __fastcall cga_lo_res_plot(cga_coord_t x, cga_coord_t y, cga_lo_res_colour_
     // AX = x, DX = y, BX = colour
     __asm {
         .8086
-        // 1. prepare registers
         mov     cx, CGA_VIDEO_RAM_SEGMENT   ; load the VRAM segment address
         mov     es, cx                      ; transer segment into es
         xchg    bx, dx                      ; BX = y DX = colour
         shl     bx, 1                       ; turn y into a word table index
         mov     bx, CGA_ROW_OFFSETS[bx]     ; load the VRAM row offset address
         mov     cx, ax                      ; copy of x in CX
-        // 2. calculate colour and mask
         and     cx, 3h                      ; mask off 0011 lower bits i.e.mod 4 pixels per byte
         shl     cx, 1                       ; *2 as 2 colour bits per pixel
         mov     dh, 00111111b               ; load DH with pixel mask
         ror     dh, cl                      ; roll mask right (msb->lsb) x mod 4
         shr     dl, cl                      ; shift colour right similarly
-        // 3. calculate offset
         shr     ax, 1                       ; calculate column byte x / 4
         shr     ax, 1                       ; 8086 limited to single step shifts                       ;
         add     bx, ax                      ; add in column byte
-        // 4. plot point
         and		es:[bx], dh		            ; mask out the pixel bits
         or		es:[bx], dl		            ; or in the pixel colour
     }
