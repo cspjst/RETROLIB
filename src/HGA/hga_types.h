1be814be7b8a0d0a2cf52c638de498911139e9ce
Sauce Code... <jeremy.thornton@nhs.net>
2026-04-24 13:47:19 +0100

    Create hga_types.h

---
+++ src/HGA/hga_types.h
@@ -0,0 +1,12 @@
+/**
+ * @author      Jeremy Simon Thornton
+ * @copyright   2026 Jeremy Simon Thornton
+ * @version     0.1.0
+ */
+#ifndef HGA_TYPES_H
+#define HGA_TYPES_H
+
+typedef unsigned short hga_size_t;
+typedef unsigned short hag_coord_t;
+
+#endif
