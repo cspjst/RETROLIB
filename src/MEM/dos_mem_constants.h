cbb2648837e1af587e96bfd9649018dc99f76e01
Hellorld! <jeremy.thornton@nhs.net>
2026-06-21 00:09:32 +0100

    test pool

---
+++ src/MEM/dos_mem_constants.h
@@ -0,0 +1,9 @@
+#ifndef DOS_MEM_CONSTANTS_H
+#define DOS_MEM_CONSTANTS_H
+
+#define DOS_MEM_1K      1024
+#define DOS_MEM_2K      2048
+#define DOS_MEM_640K    65535
+#define DOS_MEM_1MB
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,5 @@
 #include "TEST/test_pool.h"
 
-todo:
-[ ] dos_mem_constants
-[ ] delete dos max mem alloc
-
 int main() {
     test_pool();
     return 0;
