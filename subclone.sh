26c44b88dbab53b2463187a558d02489e9191e6b
Hellorld! <jeremy.thornton@nhs.net>
2026-09-08 22:30:41 +0100

    subpull

--- subclone.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-echo "Cloning the DOSLIB and BIOSLIB submodules"
-git clone --recurse-submodules https://github.com/cspjst/DOSLIB.git
--- subpull.sh
+++ subpull.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
-set -e
-echo "Updating BIOSLIB inside DOSLIB..."
-git -C doslib/bioslib checkout main
-git -C doslib submodule update --remote --merge
-git -C doslib add bioslib
-git -C doslib commit -m "Sync BIOSLIB to latest" || echo "BIOSLIB already up to date"
-
-echo "Updating DOSLIB..."
-git -C doslib checkout main
-git -C doslib add -A
-git -C doslib commit -m "Sync submodules" || echo "DOSLIB already up to date"
-
-echo "Staging DOSLIB in RETROLIB..."
-git add doslib
-git commit -m "Sync DOSLIB to latest" || echo "RETROLIB already up to date"
-
-echo "Done."
+#!/bin/sh
+# run inside dosfxp or retrolib to swap embedded doslib/ for a real submodule
+rm -rf doslib
+git submodule add https://github.com/cspjst/DOSLIB.git doslib
+git submodule update --init --recursive
+git add .gitmodules doslib
+git commit -m "Add doslib as a proper submodule"
