4e5a422450485cff246eb4ab1fda53d867bfd547
Sauce Code... <jeremy.thornton@nhs.net>
2026-09-25 14:02:14 +0100

    Refactor sim_particle_t structure
    Replaced vector properties with kinetic struct and added new fields.

--- src/SIM/sim_particle.h
+++ src/SIM/sim_particle.h
@@ -1,12 +1,11 @@
 #ifndef SIM_PARTICLE_H
 #define SIM_PARTICLE_H
 
-#include "../FXP/VEC/fxp_vec2_types.h"
+#include "sim_kinetic.h
 
 typedef struct {
-    fxp_vector2D_t position;
-    fxp_vector2D_t velocity;
-    fxp_vector2D_t acceleration;    
+    sim_kinetic_t kinetic;
+    life span, mass, radius 
 } sim_particle_t;
 
 #endif
