bb958a3308bffdbb4fe048ce1870aa2a231fa2af
Sauce Code... <jeremy.thornton@nhs.net>
2026-09-25 11:47:50 +0100

    Add sim_particle_t structure with vector types

--- src/SIM/sim_particle.h
+++ src/SIM/sim_particle.h
@@ -1,4 +1,12 @@
 #ifndef SIM_PARTICLE_H
 #define SIM_PARTICLE_H
 
+#include "../FXP/VEC/fxp_vec2_types.h"
+
+typedef struct {
+    fxp_vector2D_t position;
+    fxp_vector2D_t velocity;
+    fxp_vector2D_t acceleration;    
+} sim_particle_t;
+
 #endif
