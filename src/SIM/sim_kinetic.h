7a673cb1f0709d00f93c2492254f6cfcc8b21bc6
Sauce Code... <jeremy.thornton@nhs.net>
2026-09-25 14:00:45 +0100

    Add documentation and sim_kinetic_t struct
    Added documentation for sim_kinetic.h and included a new struct for kinetic motion.

--- src/SIM/sim_kinetic.h
+++ src/SIM/sim_kinetic.h
@@ -1,6 +1,19 @@
+/**
+ * @author      Jeremy Simon Thornton
+ * @copyright   2026 Jeremy Simon Thornton
+ * Generic motion with vectors type
+ */
 #ifndef SIM_KINETIC_H
 #define SIM_KINETIC_H
 
+#include "../FXP/VEC/fxp_vec2_types.h"
 
+typedef struct {
+    fxp_vector2D_t position;
+    fxp_vector2D_t velocity;
+    fxp_vector2D_t acceleration;    
+} sim_kinetic_t;
+
+int sim_kinetic_update(sim_kinetic_t* k);
 
 #endif 
